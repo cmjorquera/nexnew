5df9f71f6fd6e325908fb966d16d267b2a647fcf
Mirko <mirkoivanovich@yahoo.es>
2019-08-06 13:35:19 -0400

    Añadido codigo

---
+++ testeoGit/src/main/Main.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+/**
+ *
+ * @author Informatica
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int xd = 30;
+        int i = xd + 50;
+        int resultado = i + xd;
+        
+        System.out.println("Sendo código.");
+        System.out.println(resultado);
+    }
+    
+}
